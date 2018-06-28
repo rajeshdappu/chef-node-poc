0262c2ac6fa915e34e860d91babea19f94e7289d
Rajesh Dappu <rajesh.4ramesh@gmail.com>
2018-06-28 18:20:32 +0530

    cahnges

--- chef-node-poc/recipes/default.rb
+++ chef-node-poc/recipes/default.rb
@@ -7,3 +7,14 @@
 file "#{ENV['HOME']}/test.txt" do
     content 'This file was created by Chef!'
 end
+
+execute 'apt-get-update' do
+    command 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5'
+    command 'echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list'
+    command 'sudo apt-get update'
+    command 'sudo apt-get install -y mongodb-org'
+    
+    ignore_failure true
+    only_if { apt_installed? }
+    not_if { File.exist?('/var/lib/apt/periodic/update-success-stamp') }
+end
