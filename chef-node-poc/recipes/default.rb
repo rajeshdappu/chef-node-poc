89863a526f43b6b1d0631638ad992a323362f71f
Rajesh Dappu <rajesh.4ramesh@gmail.com>
2018-06-28 18:26:12 +0530

    cahnges

--- chef-node-poc/recipes/default.rb
+++ chef-node-poc/recipes/default.rb
@@ -9,6 +9,7 @@ file "#{ENV['HOME']}/test.txt" do
 end
 
 execute 'install-mongo' do
+    command 'sudo -sH'
     command 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5'
     command 'echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list'
     command 'sudo apt-get update'
